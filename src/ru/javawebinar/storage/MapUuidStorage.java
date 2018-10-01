03bada25bee71f93ad23f87ea64a1eb54a62bee0
TheMescaline <lex.korovin@gmail.com>
2018-10-01 16:13:46 +0300

    changed name for Map storage

--- src/ru/javawebinar/storage/MapUuidStorage.java
+++ src/ru/javawebinar/storage/MapUuidStorage.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapStorage extends AbstractStorage {
+public class MapUuidStorage extends AbstractStorage {
     private final Map<String, Resume> storage = new HashMap<>();
 
     @Override
--- test/ru/javawebinar/storage/SuiteTestClass.java
+++ test/ru/javawebinar/storage/SuiteTestClass.java
@@ -5,6 +5,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, MapStorageTest.class, ListStorageTest.class})
+@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, MapUuidStorageTest.class, ListStorageTest.class})
 public class SuiteTestClass {
 }
