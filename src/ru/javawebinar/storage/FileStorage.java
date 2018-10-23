fdb2c7b8c01ea8935879dbf845f9d4f087289ffc
TheMescaline <lex.korovin@gmail.com>
2018-10-23 23:14:12 +0300

    HW9 - removed Abstract classes for File & Path

--- src/ru/javawebinar/storage/FileStorage.java
+++ src/ru/javawebinar/storage/FileStorage.java
@@ -1,26 +1,110 @@
 package ru.javawebinar.storage;
 
+import ru.javawebinar.exception.StorageException;
 import ru.javawebinar.model.Resume;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class FileStorage extends AbstractFileStorage {
+public class FileStorage extends AbstractStorage<File> {
     private final IOStrategy strategy;
+    private final File directory;
 
     public FileStorage(File directory, IOStrategy strategy) {
-        super(directory);
+        Objects.requireNonNull(directory, "Directory must mot be null");
+        Objects.requireNonNull(strategy, "Strategy must mot be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory.");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable.");
+        }
+        this.directory = directory;
         this.strategy = strategy;
     }
 
     @Override
-    protected void writeResume(Resume resume, OutputStream outputStream) throws IOException {
-        strategy.writeResume(resume, outputStream);
+    protected void saveResume(Resume resume, File file) {
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new StorageException("Can not create file" + file.getAbsolutePath(), file.getName(), e);
+        }
+        updateResume(resume, file);
     }
 
     @Override
-    protected Resume readResume(InputStream inputStream) throws IOException {
+    protected void updateResume(Resume resume, File file) {
+        try {
+            writeResume(resume, new BufferedOutputStream(new FileOutputStream(file)));
+        } catch (IOException e) {
+            throw new StorageException("File write error!", resume.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected Resume getResume(File file) {
+        try {
+            return readResume(new BufferedInputStream(new FileInputStream(file)));
+        } catch (IOException e) {
+            throw new StorageException("File read error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected void deleteResume(File file) {
+        file.delete();
+    }
+
+    @Override
+    protected File getKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected boolean checkKey(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected List<Resume> getList() {
+        List<Resume> result = new ArrayList<>();
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                result.add(getResume(file));
+            }
+        } else {
+            throw new StorageException("There are no resumes in directory!", null);
+        }
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                file.delete();
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        String[] names = directory.list();
+        if (names == null) {
+            throw new StorageException("There are no resumes in directory!", null);
+        }
+        return names.length;
+    }
+
+    private void writeResume(Resume resume, OutputStream outputStream) throws IOException {
+        strategy.writeResume(resume, outputStream);
+    }
+
+    private Resume readResume(InputStream inputStream) throws IOException {
         return strategy.readResume(inputStream);
     }
 }
--- src/ru/javawebinar/storage/PathStorage.java
+++ src/ru/javawebinar/storage/PathStorage.java
@@ -1,25 +1,106 @@
 package ru.javawebinar.storage;
 
+import ru.javawebinar.exception.StorageException;
 import ru.javawebinar.model.Resume;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class PathStorage extends AbstractPathStorage {
+public class PathStorage extends AbstractStorage<Path> {
     private final IOStrategy strategy;
+    private final Path directory;
 
-    public PathStorage(String directory, IOStrategy strategy) {
-        super(directory);
+    public PathStorage(String dir, IOStrategy strategy) {
+        Path directory = Paths.get(dir);
+        Objects.requireNonNull(directory, "Directory must mot be null");
+        Objects.requireNonNull(strategy, "Strategy must mot be null");
+        if (!Files.isDirectory(directory) || !Files.isWritable(directory) || !Files.isReadable(directory)) {
+            throw new IllegalArgumentException(dir + " is not directory or is not writable/readable");
+        }
+        this.directory = directory;
         this.strategy = strategy;
     }
 
     @Override
-    protected void writeResume(Resume resume, OutputStream outputStream) throws IOException {
-        strategy.writeResume(resume, outputStream);
+    protected void saveResume(Resume resume, Path path) {
+        updateResume(resume, path);
     }
 
     @Override
-    protected Resume readResume(InputStream inputStream) throws IOException {
+    protected void updateResume(Resume resume, Path path) {
+        try {
+            writeResume(resume, new BufferedOutputStream(new FileOutputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("Path write error!", resume.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected Resume getResume(Path path) {
+        try {
+            return readResume(new BufferedInputStream(new FileInputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("Path read error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void deleteResume(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("Can not delete path " + path.toString(), path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected Path getKey(String uuid) {
+        return directory.resolve(uuid);
+    }
+
+    @Override
+    protected boolean checkKey(Path path) {
+        return Files.exists(path);
+    }
+
+    @Override
+    protected List<Resume> getList() {
+        List<Resume> result = new ArrayList<>();
+        try {
+            Files.list(directory).forEach(p -> result.add(getResume(p)));
+        } catch (IOException e) {
+            throw new StorageException("There are no resumes in directory!", null);
+        }
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        try {
+            Files.list(directory).forEach(this::deleteResume);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error!", null, e);
+        }
+    }
+
+    @Override
+    public int size() {
+        try {
+            return (int) Files.list(directory).count();
+        } catch (IOException e) {
+            throw new StorageException("Path size error!", null, e);
+        }
+    }
+
+    private void writeResume(Resume resume, OutputStream outputStream) throws IOException {
+        strategy.writeResume(resume, outputStream);
+    }
+
+    private Resume readResume(InputStream inputStream) throws IOException {
         return strategy.readResume(inputStream);
     }
 }
