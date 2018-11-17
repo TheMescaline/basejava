d3630e188083ae35f4a8b66093f3499d320039a1
TheMescaline <lex.korovin@gmail.com>
2018-11-17 10:01:17 +0300

    HW11 - removed code copying

--- src/ru/javawebinar/MainDeadlock.java
+++ src/ru/javawebinar/MainDeadlock.java
@@ -5,29 +5,21 @@ public class MainDeadlock {
     public static final Object LOCK2 = new Object();
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            synchronized (LOCK1) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (LOCK2) {
-                }
-            }
-        });
-        Thread thread2 = new Thread(() -> {
-            synchronized (LOCK2) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (LOCK1) {
-                }
-            }
-        });
+        Thread thread1 = new Thread(() -> doubleLock(LOCK1, LOCK2));
+        Thread thread2 = new Thread(() -> doubleLock(LOCK2, LOCK1));
         thread1.start();
         thread2.start();
+    }
+
+    public static void doubleLock(Object firstLock, Object secondLock) {
+        synchronized (firstLock) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (secondLock) {
+            }
+        }
     }
 }
