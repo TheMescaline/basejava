b50641916a2af4eb80a002fbd9cd86ae6d1dbd51
TheMescaline <lex.korovin@gmail.com>
2018-11-21 10:38:58 +0300

    HW12 - changes in minValue method

--- src/ru/javawebinar/MainStreams.java
+++ src/ru/javawebinar/MainStreams.java
@@ -3,23 +3,20 @@ package ru.javawebinar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        int[] valuesForMinValue = {1, 2, 2, 3, 4, 4, 4, 5};
+        int[] valuesForMinValue = {7, 4, 1, 2, 2, 3, 4, 4, 4, 5};
         List<Integer> valuesForOddOrEven = Arrays.asList(9, -8, 1, 3, 6);
         System.out.println(minValue(valuesForMinValue));
         System.out.println(oddOrEven(valuesForOddOrEven));
     }
 
     public static int minValue(int[] values) {
-        String result = Arrays.stream(values)
+        return Arrays.stream(values)
                 .distinct()
                 .sorted()
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining());
-        return Integer.parseInt(result);
+                .reduce(0, (acc, value) -> acc * 10 + value);
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
